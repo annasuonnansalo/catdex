e313f824b9969bcaeab0b72feedc3ab0a409e79c
Anna Suonansalo <anna.suonnansalo@gmail.com>
2020-05-07 17:35:01 +0100

    Added home page data rendering

--- index.php
+++ index.php
@@ -3,46 +3,16 @@
 <main>
 
     <section class="grid">
-        <a href="">
-            <picture style="background-image: url('./lib/index/ragdoll.jpg');"></picture>
-            <h2>Ragdoll</h2>
-            <p>The Ragdolls have a color point coat and blue eyes. They are large and muscular semi-longhair cats with a soft and silky coat.</p>
-        </a>
-        <a href="">
-            <picture style="background-image: url('./lib/index/angora.jpg');"></picture>
-            <h2>Turkish Angora</h2>
-            <p>Turkish Angoras are one of the ancient, natural breeds of cat, having originated in central Turkey, in the Ankara region. Documented as early as the 17th century and is believed to be the origin of the mutations for both the color white and long hair.</p>
-        </a>
-        <a href="">
-            <picture style="background-image: url('./lib/index/sphynx.jpg');"></picture>
-            <h2>Sphynx</h2>
-            <p>The Sphynx cats are known for their lack of coat. Hairlessness in cats is a naturally-occurring genetic mutation, however the Sphynx breed was developed through selective breeding, starting in the 1960s.</p>
-        </a>
-        <a href="">
-            <picture style="background-image: url('./lib/index/norwegian.jpg');"></picture>
-            <h2>Norwegian Forest cat</h2>
-            <p>Norwegian Forest cat breed is adapted to a very cold climate, with a top coat of glossy, long, water-shedding hairs and a woolly undercoat for insulation.</p>
-        </a>
-        <a href="">
-            <picture style="background-image: url('./lib/index/bengal.jpg');"></picture>
-            <h2>Bengal</h2>
-            <p>The Bengal cat is a domesticated breed created from hybrids of domestic cats, the Asian leopard cat and the Egyptian Mau, which gives them their golden shimmer.</p>
-        </a>
-        <a href="">
-            <picture style="background-image: url('./lib/index/singapura.jpg');"></picture>
-            <h2>Singapura</h2>
-            <p>The Singapura is one of the smallest breeds of cats, noted for its large eyes and ears, brown ticked coat, and blunt tail.</p>
-        </a>
-        <a href="">
-            <picture style="background-image: url('./lib/index/russian.jpg');"></picture>
-            <h2>Russian Blue</h2>
-            <p>The Russian Blue is a cat breed that comes in colors varying from a light shimmering silver to a darker, slate grey. Their short, dense coat has been the hallmark of the Russian breed for more than a century.</p>
-        </a>
-        <a href="">
-            <picture style="background-image: url('./lib/index/siamese.jpg');"></picture>
-            <h2>Siamese</h2>
-            <p>The Siamese cat is one of the first distinctly recognized breeds of Asian cat. Derived from the Wichianmat landrace, one of several varieties of cat native to China.</p>
-        </a>
+        <?php foreach (glob(__DIR__ . '/data/cat/*.json') as $file) : ?>
+            <?php $data = json_decode(file_get_contents($file)); ?>
+            <?php $href = './cat_detail.php?cat_uri=' . $data->uri; ?>
+            <a href="<?= $href; ?>">
+                <?php $image = './lib/index/' . $data->key . '.jpg'; ?>
+                <picture style="background-image: url('<?= $image; ?>');"></picture>
+                <h2><?= $data->name; ?></h2>
+                <p><?= $data->extract; ?></p>
+            </a>
+        <?php endforeach; ?>
     </section>
 
     <nav class="feature">
